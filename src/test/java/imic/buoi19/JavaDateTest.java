b5f151a0c8930e19194189870bf1eb3a2d375aee
hvdang <none@none>
2019-05-15 21:22:38 +0700

    Buoi 19: Java LocalDate example

--- src/test/java/imic/buoi19/JavaDateTest.java
+++ src/test/java/imic/buoi19/JavaDateTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class JavaDateTest
@@ -18,5 +21,30 @@ public class JavaDateTest
 
     Long timeDistance = lastDateOfMonth.getTime() - currentDate.getTime();
     System.out.println("date distance = " + timeDistance + " in miliseconds");
+  }
+
+  @Test
+  public void testLocalDateDistance()
+  {
+    LocalDate currentDate = LocalDate.now();
+    System.out.println("currentDate -> " + currentDate);
+
+    int maxDaysOfMonth = currentDate.lengthOfMonth();
+    LocalDate lastDateOfMonth = currentDate.withDayOfMonth(maxDaysOfMonth);
+    System.out.println("lastDateOfMonth -> " + lastDateOfMonth);
+
+    long dayDistance = lastDateOfMonth.toEpochDay() - currentDate.toEpochDay();
+    System.out.println("dayDistance -> " + dayDistance);
+  }
+
+  @Test
+  public void testConvertDateFromString()
+  {
+    LocalDateTime myDate = LocalDateTime.parse("2007-12-03T10:15:30");
+    System.out.println("myDate -> " + myDate);
+
+    LocalDateTime myDate2 = LocalDateTime.parse("03/12/2007 10:15:30"
+        , DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+    System.out.println("myDate2 -> " + myDate2);
   }
 }
